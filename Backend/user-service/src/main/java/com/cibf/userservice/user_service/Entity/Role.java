db9536c4474184cc0c48ee374c7df4d73ab8d81a
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2025-10-22 20:36:28 +0530

    create user data base

---
+++ Backend/user-service/src/main/java/com/cibf/userservice/user_service/Entity/Role.java
@@ -0,0 +1,6 @@
+package com.cibf.userservice.user_service.Entity;
+
+public enum Role {
+    USER,
+    EMPLOYEE
+}
---
+++ Backend/user-service/src/main/java/com/cibf/userservice/user_service/Entity/User.java
@@ -0,0 +1,54 @@
+package com.cibf.userservice.user_service.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private UUID userId;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "business_name", nullable = false)
+    private String businessName;
+
+    @Column(name = "contact_person", nullable = false)
+    private String contactPerson;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
+
